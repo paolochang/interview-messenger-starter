b9b777121b170090a7fd00a332c97d52847f65a2
Paolo Chang <chanhwan.chang@gmail.com>
2021-07-08 16:29:40 -0400

    Apply new design to login/signup pages #2

--- client/src/Login.js
+++ client/src/Login.js
@@ -9,9 +9,13 @@ import {
   FormControl,
   TextField,
 } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import { login } from "./store/utils/thunkCreators";
+import bgImage from "./assets/images/bg-img.png";
+import { ReactComponent as ChatBubble } from "./assets/images/bubble.svg";
 
 const Login = (props) => {
+  const classes = useStyles();
   const history = useHistory();
   const { user, login } = props;
 
@@ -28,16 +32,37 @@ const Login = (props) => {
   }
 
   return (
-    <Grid container justify="center">
-      <Box>
-        <Grid container item>
-          <Typography>Need to register?</Typography>
-          <Button onClick={() => history.push("/register")}>Register</Button>
+    <Grid container justify="center" className={classes.root}>
+      <Box className={classes.container}>
+        <Grid container className={classes.image}>
+          <ChatBubble className={classes.overlayIcon} />
+          <Typography className={classes.overlayText}>
+            Converse with anyone
+            <br /> with any language
+          </Typography>
         </Grid>
-        <form onSubmit={handleLogin}>
-          <Grid>
-            <Grid>
-              <FormControl margin="normal" required>
+        <div className={classes.base}>
+          <Grid container item className={classes.signup}>
+            <Typography className={classes.signupText}>
+              Don't have an account?
+            </Typography>
+            <Button
+              onClick={() => history.push("/register")}
+              className={classes.buttonSecondary}
+            >
+              Create account
+            </Button>
+          </Grid>
+          <form onSubmit={handleLogin} className={classes.form}>
+            <Grid item className={classes.innerForm}>
+              <Typography className={classes.welcomeText}>
+                Welcome back!
+              </Typography>
+              <FormControl
+                className={classes.innerForm}
+                margin="normal"
+                required
+              >
                 <TextField
                   aria-label="username"
                   label="Username"
@@ -45,22 +70,31 @@ const Login = (props) => {
                   type="text"
                 />
               </FormControl>
+              <FormControl
+                className={classes.innerForm}
+                margin="normal"
+                required
+              >
+                <TextField
+                  label="password"
+                  aria-label="password"
+                  type="password"
+                  name="password"
+                />
+              </FormControl>
+              <Grid item className={classes.button}>
+                <Button
+                  className={classes.buttonPrimary}
+                  type="submit"
+                  variant="contained"
+                  size="large"
+                >
+                  Login
+                </Button>
+              </Grid>
             </Grid>
-            <FormControl margin="normal" required>
-              <TextField
-                label="password"
-                aria-label="password"
-                type="password"
-                name="password"
-              />
-            </FormControl>
-            <Grid>
-              <Button type="submit" variant="contained" size="large">
-                Login
-              </Button>
-            </Grid>
-          </Grid>
-        </form>
+          </form>
+        </div>
       </Box>
     </Grid>
   );
@@ -79,5 +113,108 @@ const mapDispatchToProps = (dispatch) => {
     },
   };
 };
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    alignItems: "center",
+    height: "100vh",
+    backgroundColor: "#999999",
+  },
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    width: "100%",
+    height: "60%",
+    backgroundColor: "#FFFFFF",
+    [theme.breakpoints.down("sm")]: {
+      flexDirection: "column",
+      height: "100%",
+    },
+  },
+  image: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    flex: "0.35",
+    padding: "7rem 0",
+    height: "100%",
+    backgroundImage: `linear-gradient(to bottom, #3A8DFFAA, #86B9FF ),
+    url(${bgImage})`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    opacity: "85%",
+    [theme.breakpoints.down("sm")]: {
+      backgroundSize: "cover",
+    },
+  },
+  overlayIcon: {
+    color: "#FFFFFF",
+    fontWeight: "300",
+    fontSize: "5.5rem",
+    marginBottom: "2rem",
+  },
+  overlayText: {
+    color: "#FFFFFF",
+    fontSize: "1.8em",
+    textAlign: "center",
+    marginBottom: "8rem",
+  },
+  base: {
+    flex: "0.65",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  signup: {
+    width: "70%",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    marginBottom: "3rem",
+
+    [theme.breakpoints.down("sm")]: {
+      justifyContent: "center",
+    },
+  },
+  signupText: {
+    margin: "2rem",
+    color: "#AFAFAF",
+  },
+  buttonSecondary: {
+    fontSize: "1em",
+    padding: "1em 2em",
+    color: theme.palette.primary.main,
+    boxShadow:
+      "0 2px 8px 0 rgba(0, 0, 0, 0.2), 0 2px 2px 0 rgba(0, 0, 0, 0.19)",
+  },
+  form: {
+    width: "70%",
+  },
+  innerForm: {
+    width: "90%",
+  },
+  welcomeText: {
+    fontSize: "2em",
+    fontWeight: "600",
+  },
+  button: {
+    display: "flex",
+    justifyContent: "center",
+    margin: "4em",
+  },
+  buttonPrimary: {
+    fontSize: "1em",
+    fontWeight: "600",
+    padding: "1em 5.5em",
+    backgroundColor: theme.palette.primary.main,
+    color: "#FFFFFF",
+    border: "none",
+  },
+}));
 
 export default connect(mapStateToProps, mapDispatchToProps)(Login);
--- client/src/Signup.js
+++ client/src/Signup.js
@@ -10,9 +10,13 @@ import {
   TextField,
   FormHelperText,
 } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import { register } from "./store/utils/thunkCreators";
+import bgImage from "./assets/images/bg-img.png";
+import { ReactComponent as ChatBubble } from "./assets/images/bubble.svg";
 
 const Login = (props) => {
+  const classes = useStyles();
   const history = useHistory();
   const { user, register } = props;
   const [formErrorMessage, setFormErrorMessage] = useState({});
@@ -37,16 +41,33 @@ const Login = (props) => {
   }
 
   return (
-    <Grid container justify="center">
-      <Box>
-        <Grid container item>
-          <Typography>Need to log in?</Typography>
-          <Button onClick={() => history.push("/login")}>Login</Button>
+    <Grid container justify="center" className={classes.root}>
+      <Box className={classes.container}>
+        <Grid item className={classes.image}>
+          <ChatBubble className={classes.overlayIcon} />
+          <Typography className={classes.overlayText}>
+            Converse with anyone
+            <br /> with any language
+          </Typography>
         </Grid>
-        <form onSubmit={handleRegister}>
-          <Grid>
-            <Grid>
-              <FormControl>
+        <Grid className={classes.base}>
+          <Grid container item className={classes.login}>
+            <Typography className={classes.loginText}>
+              Already have an account?
+            </Typography>
+            <Button
+              onClick={() => history.push("/login")}
+              className={classes.buttonSecondary}
+            >
+              Login
+            </Button>
+          </Grid>
+          <form onSubmit={handleRegister} className={classes.form}>
+            <Grid className={classes.innerForm}>
+              <Typography className={classes.createText}>
+                Create an account.
+              </Typography>
+              <FormControl className={classes.innerForm}>
                 <TextField
                   aria-label="username"
                   label="Username"
@@ -55,9 +76,7 @@ const Login = (props) => {
                   required
                 />
               </FormControl>
-            </Grid>
-            <Grid>
-              <FormControl>
+              <FormControl className={classes.innerForm}>
                 <TextField
                   label="E-mail address"
                   aria-label="e-mail address"
@@ -66,9 +85,10 @@ const Login = (props) => {
                   required
                 />
               </FormControl>
-            </Grid>
-            <Grid>
-              <FormControl error={!!formErrorMessage.confirmPassword}>
+              <FormControl
+                className={classes.innerForm}
+                error={!!formErrorMessage.confirmPassword}
+              >
                 <TextField
                   aria-label="password"
                   label="Password"
@@ -81,9 +101,10 @@ const Login = (props) => {
                   {formErrorMessage.confirmPassword}
                 </FormHelperText>
               </FormControl>
-            </Grid>
-            <Grid>
-              <FormControl error={!!formErrorMessage.confirmPassword}>
+              <FormControl
+                className={classes.innerForm}
+                error={!!formErrorMessage.confirmPassword}
+              >
                 <TextField
                   label="Confirm Password"
                   aria-label="confirm password"
@@ -96,12 +117,19 @@ const Login = (props) => {
                   {formErrorMessage.confirmPassword}
                 </FormHelperText>
               </FormControl>
+              <Grid className={classes.button}>
+                <Button
+                  className={classes.buttonPrimary}
+                  type="submit"
+                  variant="contained"
+                  size="large"
+                >
+                  Create
+                </Button>
+              </Grid>
             </Grid>
-            <Button type="submit" variant="contained" size="large">
-              Create
-            </Button>
-          </Grid>
-        </form>
+          </form>
+        </Grid>
       </Box>
     </Grid>
   );
@@ -120,5 +148,107 @@ const mapDispatchToProps = (dispatch) => {
     },
   };
 };
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    alignItems: "center",
+    height: "100vh",
+    backgroundColor: "#999999",
+  },
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    width: "100%",
+    height: "60%",
+    backgroundColor: "#FFFFFF",
+    [theme.breakpoints.down("sm")]: {
+      flexDirection: "column",
+      height: "100%",
+    },
+  },
+  image: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    flex: "0.35",
+    padding: "7rem 0",
+    backgroundImage: `linear-gradient(to bottom, #3A8DFFAA, #86B9FF ),
+    url(${bgImage})`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    opacity: "85%",
+  },
+  overlayIcon: {
+    color: "#FFFFFF",
+    fontWeight: "300",
+    fontSize: "5.5rem",
+    marginBottom: "2rem",
+  },
+  overlayText: {
+    color: "#FFFFFF",
+    fontSize: "1.8em",
+    textAlign: "center",
+    marginBottom: "8rem",
+  },
+  base: {
+    flex: "0.65",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  login: {
+    width: "70%",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    marginBottom: "3rem",
+
+    [theme.breakpoints.down("sm")]: {
+      justifyContent: "center",
+    },
+  },
+  loginText: {
+    margin: "2rem",
+    color: "#AFAFAF",
+  },
+  buttonSecondary: {
+    fontSize: "1em",
+    padding: "1em 4em",
+    color: theme.palette.primary.main,
+    boxShadow:
+      "0 2px 8px 0 rgba(0, 0, 0, 0.2), 0 2px 2px 0 rgba(0, 0, 0, 0.19)",
+  },
+  form: {
+    width: "70%",
+  },
+  createText: {
+    fontSize: "2em",
+    fontWeight: "600",
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "1.5em",
+    },
+  },
+  innerForm: {
+    width: "90%",
+  },
+  button: {
+    display: "flex",
+    justifyContent: "center",
+    margin: "4em",
+  },
+  buttonPrimary: {
+    fontSize: "1em",
+    fontWeight: "600",
+    padding: "1em 5em",
+    backgroundColor: theme.palette.primary.main,
+    color: "#FFFFFF",
+    border: "none",
+  },
+}));
 
 export default connect(mapStateToProps, mapDispatchToProps)(Login);
