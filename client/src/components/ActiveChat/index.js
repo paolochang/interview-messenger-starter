ac3dfbfee695462b4f84c2331f9a708fbcc2e264
Paolo Chang <chanhwan.chang@gmail.com>
2021-07-12 21:29:07 -0400

    Add new feature: sending images #3

--- client/src/components/ActiveChat/index.js
+++ client/src/components/ActiveChat/index.js
@@ -1,5 +1,6 @@
 export { default as SenderBubble } from "./SenderBubble";
 export { default as OtherUserBubble } from "./OtherUserBubble";
+export { default as ImageBubble } from "./ImageBubble";
 export { default as ActiveChat } from "./ActiveChat";
 export { default as Input } from "./Input";
 export { default as Header } from "./Header";
